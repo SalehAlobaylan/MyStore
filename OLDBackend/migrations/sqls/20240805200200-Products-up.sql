030f9a994a37eed39f9c38eb66d779befbfd47a6
Salehalobilan <salehwleed1@gmail.com>
2025-02-27 17:48:11 +0300

    deleting oldBackend

--- OLDBackend/migrations/sqls/20240805200200-Products-up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE Products (id VARCHAR(150) PRIMARY KEY, name VARCHAR(150), price INTEGER, category VARCHAR(150));
-INSERT INTO Products(id, name, price, category) VALUES('1111', 'for', 1, 'testing') RETURNING *;
--- OLDBackend/migrations/sqls/20240806201634-users-up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE users(id VARCHAR(150) PRIMARY KEY, firstName VARCHAR(150), lastName VARCHAR(150), password VARCHAR(150));
-INSERT INTO users(id, firstName, lastName, password) VALUES('1111','for', 'testing', 'only') RETURNING *;
